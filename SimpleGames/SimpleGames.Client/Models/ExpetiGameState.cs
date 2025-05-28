595d0b9fa15f52e944dbb7813698a44a22b9797d
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-28 13:58:13 -0500

    error hunting

--- SimpleGames/SimpleGames.Client/Models/ExpetiGameState.cs
+++ SimpleGames/SimpleGames.Client/Models/ExpetiGameState.cs
@@ -4,51 +4,42 @@ namespace SimpleGames.Client.Models
 {
     public class ExpetiGameState
     {
-        static int targetNumber { get; set; }
-        static List<int> guesses = new List<int>();
-        static bool stillGuessing = true;
+        public int TargetNumber { get; set; }
+        public List<int> Guesses { get; set; } = new List<int>();
+        public bool StillGuessing { get; set; } = true;
 
         public ExpetiGameState() { }
 
         public void Start()
         {
-            // update logic from CSharp players guide source code
-        }
-
-        public void Greeting()
-        {
-            // update logic from CSharp players guide source code
+            PickRandomNumber();     // start game by picking new target number 
+            Guesses.Clear();        // clear any guesses from previous games
+            StillGuessing = true;   // reset game state to true 
         }
 
         public void PickRandomNumber()
         {
             Random random = new Random();
-            targetNumber = random.Next(10);
+            TargetNumber = random.Next(10);
 
         }
 
         public bool IsValidGuess(int guess)
         {
-            if (guesses.Contains(guess)) return false;
+            if (Guesses.Contains(guess)) return false;
 
             return true;
         }
 
         public void TrackPlayerGuess(int guess)
         {
-            guesses.Add(guess);
+            Guesses.Add(guess);
         }
 
         public void GameOver()
         {
-            Console.WriteLine("\nThanks for playing");
+            StillGuessing = false;
         }
-
-        public void CollectPlayerGuess()
-        {
-            // update logic from CSharp players guide source code
-        }
-
 
     }
 }
