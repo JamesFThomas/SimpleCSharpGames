05b77f5e706d04e34e478c080f5f83c6e38fcdd2
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-28 09:54:30 -0500

    Expeti's Game - Game component and classes outlined

---
+++ SimpleGames/SimpleGames.Client/Models/ExpetiGameState.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+
+namespace SimpleGames.Client.Models
+{
+    public class ExpetiGameState
+    {
+        static int targetNumber { get; set; }
+        static List<int> guesses = new List<int>();
+        static bool stillGuessing = true;
+
+        public ExpetiGameState() { }
+
+        public void Start()
+        {
+            // update logic from CSharp players guide source code
+        }
+
+        public void Greeting()
+        {
+            // update logic from CSharp players guide source code
+        }
+
+        public void PickRandomNumber()
+        {
+            Random random = new Random();
+            targetNumber = random.Next(10);
+
+        }
+
+        public bool IsValidGuess(int guess)
+        {
+            if (guesses.Contains(guess)) return false;
+
+            return true;
+        }
+
+        public void TrackPlayerGuess(int guess)
+        {
+            guesses.Add(guess);
+        }
+
+        public void GameOver()
+        {
+            Console.WriteLine("\nThanks for playing");
+        }
+
+        public void CollectPlayerGuess()
+        {
+            // update logic from CSharp players guide source code
+        }
+
+
+    }
+}
---
+++ SimpleGames/SimpleGames.Client/Models/ExpetiPlayer.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleGames.Client.Models
+{
+    public class ExpetiPlayer
+    {
+        public int Id;
+        public ExpetiPlayer(int id)
+        {
+            Id = id;
+        }
+
+        public int Guess()
+        {
+            // update logic from CSharp players guide source code
+
+            return 1;
+        }
+    }
+}
